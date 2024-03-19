90e48e79e81d8bc1d5683cdb47bdc63f085d988f
Pedro Augusto <apedro.rp@gmail.com>
2024-03-19 14:08:30 -0300

    feat: dark mode, fix: function valorDaAposta, botao reiniciar jogo e botao apostar

--- SandBox/MegaSena/src/js/megaSena.js
+++ SandBox/MegaSena/src/js/megaSena.js
@@ -9,9 +9,40 @@ btnApostar.disabled = true;
 
 sortearNumeros();
 
+// #region Alterar Tema
+
+// Obtenha a entrada de alternância do tema
+const themeToggle = document.querySelector(
+    '.switch input[type="checkbox"]'
+);
+// Função que irá mudar o tema com base em se a alternância do tema está marcada ou não
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+    }
+}
+// Adicione um ouvinte de evento ao alternador de tema, o que mudará o tema
+themeToggle.addEventListener("change", switchTheme, false);
+
+// Obtenha o tema atual do armazenamento local
+const currentTheme = localStorage.getItem("theme");
+// se o item de armazenamento local atual pode ser encontrado
+if (currentTheme) {
+    //  Defina o atributo body data-theme para corresponder ao item de armazenamento local
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    // Se o tema atual estiver escuro, verifique a alternância do tema
+    if (currentTheme === "dark") {
+        themeToggle.checked = true;
+    }
+}
+
+// #endregion
+
 function sortearNumeros() {  // otimizar essa funcao mais tarde
     for (i = 0; i < 6; i++) {
-        let numeroSorteado = Math.round(Math.random() * 59 + 1);
+        let numeroSorteado = Math.round(Math.random() * 59 + 1); //Math.floor tentar mais tarde
 
         // verifica se o número sorteado exite na lista, enquanto exixtir ele vai sortear um nopvo número
         while (resultado.includes(numeroSorteado)) {
@@ -34,7 +65,7 @@ function selecionarNumeros(numero) {
         if (numerosApostados.length > 5) {
             btnApostar.disabled = false;
 
-            valorAposta();
+            valorDaAposta();
         };
 
         // mostra quantidade de numeros apostados
@@ -52,7 +83,7 @@ function desabilitarNumeroEscolhido(numero) {
 
 };
 
-function valordaAposta() { // nao funciona - ver 
+function valorDaAposta() {
     switch (numerosApostados.length) {
         case 6:
             valorAposta = "R$ 5,00"
@@ -129,17 +160,31 @@ function apostar() {
 
     //habilitar o botao reiniciar
     document.getElementById("btnReiniciar").style.display = 'inline';
+    document.getElementById('btnApostar').disabled = true
 
 };
 
 function desabilitarTodosNumeros() {
-    for (i = 0; i <= 60; i++)
+    for (i = 1; i <= 60; i++) {
         document.getElementById("num_" + i).disabled = true;
-
-
+    }
 }
 
 let btn = document.querySelector("#btnReiniciar");
 btn.addEventListener("click", function () {
     location.reload();
 });
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+
+        // Defina a preferência de tema do usuário como escuro
+        localStorage.setItem("theme", "dark");
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+
+        // Defina a preferência de tema do usuário como claro
+        localStorage.setItem("theme", "light");
+    }
+}
