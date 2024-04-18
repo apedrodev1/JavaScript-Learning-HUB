cbf7e9ef9444ab382c3eb5f87262d00038d69e1b
Pedro Augusto <apedro.rp@gmail.com>
2024-04-18 10:45:55 -0300

    feat: atribuição de um ID unico por contato

---
+++ Lista Telefônica/src/js/script.js
@@ -0,0 +1,52 @@
+const contacts = {};
+const contactID = Math.random().toString(36).substring(2, 11);
+
+while (true) {
+    let resposta = prompt("Olá, deseja cadastrar seu contato? (0 - Não, 1 - Sim)");
+
+    if (resposta === '1') {
+        let firstName = prompt("Por favor, insira seu primeiro nome: ");
+        if (!firstName) {
+            alert("O primeiro nome é obrigatório!");
+            continue;
+        }
+
+        let surName = prompt("E qual é o seu sobrenome?");
+
+        let phoneNumber = prompt("Agora, por favor, insira o seu telefone:");
+        if (!phoneNumber) {
+            alert("O número de telefone é obrigatório!");
+            continue;
+        }
+
+        if (!phoneNumber.match(/^\d{10}$/)) {
+            alert("Por favor, insira um número de telefone válido (10 dígitos).");
+            continue;
+        }
+
+        let emailAdress = prompt("E o seu e-mail:");
+
+
+        if (emailAdress && !emailAdress.includes('@')) {
+            alert("Por favor, insira um endereço de e-mail válido.");
+            continue;
+        }
+
+        let contato = {
+            fullName: firstName + " " + surName,
+            phoneNumber: phoneNumber,
+            emailAdress: emailAdress
+        };
+
+        contacts[contactID] = contato;
+
+        alert("Muito obrigado, " + firstName + "! Seu contato foi cadastrado no nosso banco de dados! Seu número de indentificação é: " + contactID);
+    } else if (resposta === '0') {
+        alert("Cadastro cancelado.");
+        break;
+    } else {
+        alert("Opção inválida. Por favor, digite 0 para Não ou 1 para Sim.");
+    }
+}
+
+console.log("Contatos cadastrados:", contacts);
