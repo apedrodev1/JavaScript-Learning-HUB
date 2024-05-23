c0edd14a164b2fc92bf5a1958b3666a1ba5f03a1
Pedro Augusto <apedro.rp@gmail.com>
2024-05-23 10:42:25 -0300

    recfator: funcao piscaBotao e modal

--- sandBox/Jogo Forca/src/js/script.js
+++ sandBox/Jogo Forca/src/js/script.js
@@ -319,7 +319,7 @@ async function comparaListas(letra) {
         tentativas--;
         carregaImgForca();
         if (tentativas == 0) {
-            abreModal("OPS!, não foi dessa vez... A palavra secreta era <br>" + palavraSecretaSorteada);
+            abreModal("OPS! Não foi dessa vez... A palavra secreta era <br>" + palavraSecretaSorteada);
             piscarBotao();
         }
 
@@ -389,20 +389,22 @@ async function comparaListas(letra) {
 
         }
     }
-
-    function abreModal() {
-        $('#myModal').modal({
-            show: true
-        });
-
-        let bntReiniciar = document.querySelector("#btnReiniciar")
-        bntReiniciar.addEventListener("click", function () {
-            restart = false;
-            location.reload();
-        });
-    }
 }
 
+function abreModal(titulo, mensagem) {
+    let modalTitulo = document.getElementById("modalLabel");
+    modalTitulo.innerText = titulo;
 
+    let modalBody = document.getElementById("modalBody");
+    modalBody.innerHTML = mensagem;
 
+    $('#myModal').modal({
+        show: true
+    });
 
+    let bntReiniciar = document.querySelector("#btnReiniciar")
+    bntReiniciar.addEventListener("click", function () {
+        location.reload();
+        restart = false;
+    });
+}
