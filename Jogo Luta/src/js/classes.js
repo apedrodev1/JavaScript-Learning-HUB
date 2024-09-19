54349fe2ab3f9ebd37f0b4176e3f616f09edbfea
Pedro <apedro.rp@gmail.com>
2024-09-19 19:58:33 -0300

    refactor: function doAttack, feat: function endFight

--- Jogo Luta/src/js/classes.js
+++ Jogo Luta/src/js/classes.js
@@ -1,67 +1,3 @@
-class Character {
-
-    _life = 1;
-    maxLife = 1;
-    attack = 0;
-    defense = 0;
-    alive = true; //variavel que verifica se o personagem esta ou nao vivo
-
-    constructor(name) {
-        this.name = name;
-    }
-
-    get life() {
-        return this._life;
-    }
-    set life(newLife) {
-        this._life = newLife < 0 ? 0 : newLife;
-    }
-}
-
-class Knight extends Character {
-    constructor(name) {
-        super(name);
-        this.life = 100;
-        this.attack = 10;
-        this.defense = 8;
-        this.maxLife = this.life;
-        this.alive = this.alive;
-    }
-}
-
-class Sorcerer extends Character {
-    constructor(name) {
-        super(name);
-        this.life = 80;
-        this.attack = 15;
-        this.defense = 3;
-        this.maxLife = this.life;
-        this.alive = this.alive;
-    }
-}
-
-class LittleMonster extends Character {
-    constructor() {
-        super('Little Monster');
-        this.life = 60;
-        this.attack = 5;
-        this.defense = 4;
-        this.maxLife = this.life
-        this.alive = this.alive;
-    }
-}
-
-class BigMonster extends Character {
-    constructor() {
-        super('Big Monster');
-        this.life = 120;
-        this.attack = 16;
-        this.defense = 5;
-        this.maxLife = this.life
-        this.alive = this.alive;
-    }
-}
-
 class Stage {
     constructor(fighter1, fighter2, fighter1El, fighter2El, logObject) {
         this.fighter1 = fighter1;
@@ -69,15 +5,25 @@ class Stage {
         this.fighter1El = fighter1El;
         this.fighter2El = fighter2El;
         this.log = logObject;
+        this.isGameOver = false;
 
     }
 
     start() {
         this.update();
 
+
+        //Evento attackButton 
         this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
 
         this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
+
+
+        //evento healButton
+        this.fighter1El.querySelector('.healButton').addEventListener('click', () => this.doHeal(this.fighter1));
+
+        this.fighter2El.querySelector('.healButton').addEventListener('click', () => this.fighter2);
+
 
     }
 
@@ -91,11 +37,11 @@ class Stage {
         this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
         let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
         this.fighter2El.querySelector('.bar').style.width = `${f2Pct.toFixed(2)}%`;
+
     }
 
     doAttack(attacking, attacked) {
-        if (attacking.life <= 0 || attacked.life <= 0) { //condicao de vitoria ou derrota
-            this.log.addMessage('Atacando cachorro morto');
+        if (attacking.life <= 0 || attacked.life <= 0) { // Condição de vitória ou derrota
             return;
         }
 
@@ -112,12 +58,27 @@ class Stage {
             this.log.addMessage(`${attacked.name} defendeu o ataque`);
         }
 
-        // this.log.addMessage(`${attacking.name} esta atacando ${attacked.name}`)
-
+        // Verificação do fim da luta
+        if (attacked.life <= 0) {
+            this.log.addMessage(`${attacked.name} foi derrotado!`);
+            this.endFight(attacking.name); // Chama a função para terminar a luta
+            return; // Finaliza a função se um dos jogadores foi derrotado
+        }
 
         this.update();
-
     }
+
+    endFight(winnerName) {
+        this.log.addMessage(`${winnerName} venceu a luta!`);
+        // Desabilitar botões de ataque
+        this.fighter1El.querySelector('.attackButton').disabled = true;
+        this.fighter2El.querySelector('.attackButton').disabled = true;
+
+        this.fighter1El.querySelector('.healButton').disabled = true;
+        this.fighter2El.querySelector('.healButton').disabled = true;
+    }
+
+
 }
 
 class Log {
