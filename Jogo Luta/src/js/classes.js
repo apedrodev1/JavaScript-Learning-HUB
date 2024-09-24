0d1ecf7db44cce359bbbad0a19aa211b5b031305
Pedro <apedro.rp@gmail.com>
2024-09-24 00:20:55 -0300

    feat: functions updateLifeBar, updateHearts

--- Jogo Luta/src/js/classes.js
+++ Jogo Luta/src/js/classes.js
@@ -1,4 +1,4 @@
-class Stage {
+export class Stage {
     constructor(fighter1, fighter2, fighter1El, fighter2El, logObject, controllers) {
         this.fighter1 = fighter1;
         this.fighter2 = fighter2;
@@ -7,21 +7,23 @@ class Stage {
         this.log = logObject;
         this.isGameOver = false;
         this.controllers = controllers;
+        this.count = 0;
     }
 
     start() {
         this.update();
         this.controlTurns();
         this.controllers.initialize(this);
+        this.updateHearts(this.fighter1El, this.fighter1.healingTimes)
+        this.updateHearts(this.fighter2El, this.fighter2.healingTimes)
     }
 
-    controlTurns() {     //quase finalizada - apenas retirar scale do botao attack de quem espera -- .style.transform = 'scale(1)'
+    controlTurns() {
         const fighter1AttackButton = this.controllers.fighter1AttackButton;
         const fighter2AttackButton = this.controllers.fighter2AttackButton;
         const healer1Button = this.controllers.healer1Button;
         const healer2Button = this.controllers.healer2Button;
 
-        // Desativando os botões de ataque e cura inicialmente
         fighter1AttackButton.disabled = true;
         fighter2AttackButton.disabled = true;
         healer1Button.disabled = true;
@@ -35,63 +37,26 @@ class Stage {
 
         this.log.addMessage(message);
 
-        // Delay de 2 segundos antes de liberar os botões
         setTimeout(() => {
             starter.disabled = false;
             starterHealer.disabled = false;
         }, 2000);
-
-        // Configuração dos turnos
-        fighter1AttackButton.addEventListener('click', () => {
-            fighter1AttackButton.disabled = true;
-            fighter2AttackButton.disabled = false;
-            healer1Button.disabled = true; // Desabilita o botão de cura após atacar
-            healer2Button.disabled = false; // Permite que o segundo jogador cure
-
-            // Ativa o cursor do próximo jogador
-            fighter2AttackButton.style.cursor = 'pointer';
-            fighter1AttackButton.style.cursor = 'not-allowed'; // Desativa o cursor do jogador 1
-        });
-
-        fighter2AttackButton.addEventListener('click', () => {
-            fighter2AttackButton.disabled = true;
-            fighter1AttackButton.disabled = false;
-            healer2Button.disabled = true;
-            healer1Button.disabled = false;
-
-            fighter1AttackButton.style.cursor = 'pointer';
-            fighter2AttackButton.style.cursor = 'not-allowed';
-        });
-
-        healer1Button.addEventListener('click', () => {
-            this.doHeal(fighter1); // Chama a função de cura
-            fighter2AttackButton.disabled = false; // Permite que o segundo jogador ataque
-
-            // Desativa o cursor do jogador 1 após curar
-            fighter1AttackButton.style.cursor = 'not-allowed';
-            healer2Button.disabled = false; // Permite que o segundo jogador cure
-        });
-
-        healer2Button.addEventListener('click', () => {
-            this.doHeal(fighter2);
-            fighter1AttackButton.disabled = false;
-
-
-            fighter2AttackButton.style.cursor = 'not-allowed';
-            healer1Button.disabled = false;
-        });
     }
 
     update() {
         // Fighter 1
-        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(2)} HP`;
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name}`;
         let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
         this.fighter1El.querySelector('.bar').style.width = `${f1Pct.toFixed(2)}%`;
+        this.updateLifeBar(this.fighter1)
 
         // Fighter 2
-        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(2)} HP`;
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name}`;
         let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
         this.fighter2El.querySelector('.bar').style.width = `${f2Pct.toFixed(2)}%`;
+        this.updateLifeBar(this.fighter2)
+
+
     }
 
     doAttack(attacking, attacked) { //adicionar a condicao dodge actualAttack = (attack + acurracy * (attackFactor)) = actualDefense = (defense + speed *(defenseFactor))
@@ -116,13 +81,12 @@ class Stage {
         if (attacked.life <= 0) {
             this.log.addMessage(`${attacked.name} foi derrotado!`);
             this.endFight(attacking.name);
-            return;
         }
 
         this.update();
     }
 
-    doHeal(fighter) { //funcao heal nao obedece a logica de uma jogada por vez / porem funciona (ajustar no futuro, talvez) / as verificacoes de vida cheia e uso 4 vezes quebrariam o controlTurns()
+    doHeal(fighter) {
         // Verifica se a vida do fighter está cheia
         if (fighter.life >= fighter.maxLife) {
             this.log.addMessage(`${fighter.name} está com a vida cheia e não pode se curar!`);
@@ -140,8 +104,8 @@ class Stage {
             fighter.healingTimes--;
 
             //manipular o elemento DOM .healChances settar coracoes - implementar
-            const healCountElement = fighter === this.fighter1 ? this.fighter1El.querySelector('.healCount') : this.fighter2El.querySelector('.healCount');
-            healCountElement.innerText = fighter.healingTimes;
+            const fighterEl = fighter === this.fighter1 ? this.fighter1El : this.fighter2El;
+            this.updateHearts(fighterEl, fighter.healingTimes);
 
             // Desabilita o botão de cura se não houver mais curas disponíveis
             if (fighter.healingTimes === 0) {
@@ -150,14 +114,11 @@ class Stage {
                     this.controllers.healer1Button.style.cursor = 'not-allowed'; // Desativa o cursor
                     this.controllers.healer1Button.style.transform = 'scale(1)';
                     this.controllers.healer1Button.style.backgroundColor = '#a6e3e3';
-                    // this.controllers.healer1.style.boxShadow = ''; tentar tirar o box-shadow
                 } else {
                     this.controllers.healer2Button.disabled = true;
                     this.controllers.healer2Button.style.cursor = 'not-allowed';
                     this.controllers.healer2Button.style.transform = 'scale(1)';
                     this.controllers.healer2Button.style.backgroundColor = '#a6e3e3';
-                    // this.controllers.healer2Button.boxShadow = '';
-
                 }
 
                 // if (this.shouldGiveExtraHeal()) {  // chama funcao para conceder mais alguns usos do botao heal durante a luta / implementar mais tarde
@@ -179,9 +140,45 @@ class Stage {
         this.fighter1El.querySelector('.healButton').disabled = true;
         this.fighter2El.querySelector('.healButton').disabled = true;
     }
+
+    updateLifeBar(fighter) {
+        const lifeBarElement = fighter === this.fighter1 ? this.fighter1El : this.fighter2El;
+        const lifeBar = lifeBarElement.querySelector('.bar');
+        const lifeText = lifeBarElement.querySelector('.lifeText');
+        const lifePercentage = (fighter.life / fighter.maxLife) * 100;
+        console.log(lifePercentage);
+
+        lifeBar.style.width = `${lifePercentage}%`;
+
+        // Muda a cor da barra com base na vida
+        if (lifePercentage > 50) {
+            lifeBar.style.backgroundColor = 'green';
+        } else if (lifePercentage > 25) {
+            lifeBar.style.backgroundColor = 'yellow';
+        } else {
+            lifeBar.style.backgroundColor = 'red';
+        }
+
+        lifeText.textContent = `${fighter.life.toFixed(2)} / ${fighter.maxLife}`; // Atualiza o texto
+    }
+
+    updateHearts(el, healingTimes) {
+
+        const heartsContainer = el.querySelector(".healChances");
+        heartsContainer.innerHTML = ""
+
+        const heartsImg = Array.from({ length: healingTimes }).map(_ => {
+            const img = document.createElement("img");
+            img.setAttribute("src", "./src/assets/img/heart.png");
+            img.setAttribute("width", "15")
+            return img;
+        })
+
+        heartsImg.forEach(img => heartsContainer.appendChild(img));
+    }
 }
 
-class Log {
+export class Log {
     list = [];
 
     constructor(listEl) {
@@ -192,29 +189,11 @@ class Log {
         this.list.push(msg);
         let li = document.createElement('li');
         li.innerText = msg;
-        this.listEl.appendChild(li); // Adiciona o <li> à lista
+        this.listEl.appendChild(li);
+        this.listEl.scrollTo(0, this.listEl.scrollHeight)
     }
 }
 
 
 
 
-// updateLifeBar(fighter) {
-//     const lifeBarElement = fighter === this.fighter1 ? this.fighter1El : this.fighter2El;
-//     const lifeBar = lifeBarElement.querySelector('.bar');
-//     const lifeText = lifeBarElement.querySelector('.lifeText');
-//     const lifePercentage = (fighter.life / fighter.maxLife) * 100;
-
-//     lifeBar.style.width = `${lifePercentage}%`;
-
-//     // Muda a cor da barra com base na vida
-//     if (lifePercentage > 50) {
-//         lifeBar.style.backgroundColor = 'green';
-//     } else if (lifePercentage > 25) {
-//         lifeBar.style.backgroundColor = 'yellow';
-//     } else {
-//         lifeBar.style.backgroundColor = 'red';
-//     }
-
-//     lifeText.textContent = `${fighter.life} / ${fighter.maxLife}`; // Atualiza o texto
-// }
