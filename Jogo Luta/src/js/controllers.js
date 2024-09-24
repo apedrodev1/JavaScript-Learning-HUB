ac308b53a41fc9fc32839ec5aa1ba48bb3a31151
Pedro <apedro.rp@gmail.com>
2024-09-24 00:23:14 -0300

    refactor: function initialize - realizando mudancas de style nos controllers

--- Jogo Luta/src/js/controllers.js
+++ Jogo Luta/src/js/controllers.js
@@ -1,4 +1,4 @@
-class Controler {
+export class Controler {
     constructor(fighter1El, fighter2El) {
         this.fighter1El = fighter1El;
         this.fighter2El = fighter2El;
@@ -16,5 +16,50 @@ class Controler {
 
         this.healer1Button.addEventListener('click', () => stage.doHeal(stage.fighter1));
         this.healer2Button.addEventListener('click', () => stage.doHeal(stage.fighter2));
+
+        // Configuração dos turnos
+        this.fighter1AttackButton.addEventListener('click', () => {
+            this.fighter1AttackButton.disabled = true;
+            this.fighter2AttackButton.disabled = false;
+            this.healer1Button.disabled = true; // Desabilita o botão de cura após atacar
+            this.healer2Button.disabled = false; // Permite que o segundo jogador cure
+
+            // Ativa o cursor do próximo jogador
+            this.fighter2AttackButton.style.cursor = 'pointer';
+            this.fighter1AttackButton.style.cursor = 'not-allowed'; // Desativa o cursor do jogador 1
+        });
+
+        this.fighter2AttackButton.addEventListener('click', () => {
+            this.fighter2AttackButton.disabled = true;
+            this.fighter1AttackButton.disabled = false;
+            this.healer2Button.disabled = true;
+            this.healer1Button.disabled = false;
+
+            this.fighter1AttackButton.style.cursor = 'pointer';
+            this.fighter2AttackButton.style.cursor = 'not-allowed';
+        });
+
+        this.healer1Button.addEventListener('click', () => {
+            this.fighter1AttackButton.disabled = true;
+            this.healer1Button.disabled = true;
+
+            this.fighter2AttackButton.disabled = false; // Permite que o segundo jogador ataque
+            this.healer2Button.disabled = false; // Permite que o segundo jogador cure
+
+            // Desativa o cursor do jogador 1 após curar
+            this.fighter1AttackButton.style.cursor = 'not-allowed';
+            this.fighter2AttackButton.style.cursor = 'pointer';
+        });
+
+        this.healer2Button.addEventListener('click', () => {
+            this.fighter1AttackButton.disabled = false;
+            this.healer1Button.disabled = false;
+
+            this.fighter2AttackButton.disabled = true; // Permite que o segundo jogador ataque
+            this.healer2Button.disabled = true; // Permite que o segundo jogador cure
+
+            this.fighter1AttackButton.style.cursor = "pointer"
+            this.fighter2AttackButton.style.cursor = 'not-allowed';
+        });
     }
 }
