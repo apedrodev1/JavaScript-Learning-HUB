73f3e67605789b9a90b42c764b4158f8d30c80e0
Pedro <apedro.rp@gmail.com>
2024-10-07 17:07:14 -0300

    feat new character Knight

---
+++ Jogo Luta/src/js/jogoLuta/characters/Knight/Knight.js
@@ -0,0 +1,14 @@
+import { Character } from "../Character.js";
+
+export class Knight extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 130;
+        this.attack = 18;
+        this.accuracy = 4;
+        this.defense = 7;
+        this.speed = 8;
+        this.maxLife = this.life;
+        this.alive = this.alive;
+    }
+}
