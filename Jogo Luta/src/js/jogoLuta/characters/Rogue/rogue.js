82d0785cd45bb96d11b15b77cf11903d765fff72
Pedro <apedro.rp@gmail.com>
2024-10-07 17:08:17 -0300

    feat new character Rogue

---
+++ Jogo Luta/src/js/jogoLuta/characters/Rogue/rogue.js
@@ -0,0 +1,14 @@
+import { Character } from "../Character";
+
+export class Rogue extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 105;
+        this.attack = 16;
+        this.accuracy = 7
+        this.defense = 8;
+        this.speed = 13;
+        this.maxLife = this.life;
+        this.alive = this.alive;
+    }
+}
