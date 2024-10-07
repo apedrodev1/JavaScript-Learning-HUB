9f38080fe85081597a03115ecf0b33fb2ed2edb7
Pedro <apedro.rp@gmail.com>
2024-10-07 17:07:47 -0300

    feat new character Orc

---
+++ Jogo Luta/src/js/jogoLuta/characters/Orc/orc.js
@@ -0,0 +1,14 @@
+import { Character } from "../Character";
+
+export class Orc extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 135;
+        this.attack = 9;
+        this.accuracy = 11
+        this.defense = 13;
+        this.speed = 8;
+        this.maxLife = this.life;
+        this.alive = this.alive;
+    }
+}
