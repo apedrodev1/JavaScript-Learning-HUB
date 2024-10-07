795beeea5ca2da9935fdcaf92313158d328c9a87
Pedro <apedro.rp@gmail.com>
2024-10-07 17:06:04 -0300

    feat new character Dwarf

---
+++ Jogo Luta/src/js/jogoLuta/characters/Dwarf/dwarf.js
@@ -0,0 +1,14 @@
+import { Character } from "../Character";
+
+export class Dwarf extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 150;
+        this.attack = 18;
+        this.accuracy = 3;
+        this.defense = 6;
+        this.speed = 2;
+        this.maxLife = this.life;
+        this.alive = this.alive;
+    }
+}
