38150444a0f5623f80c72fe855f1cf9e3982b99e
Pedro <apedro.rp@gmail.com>
2024-10-07 17:05:20 -0300

    feat new character Archer

---
+++ Jogo Luta/src/js/jogoLuta/characters/Archer/archer.js
@@ -0,0 +1,14 @@
+import { Character } from "../Character";
+
+export class Sorcerer extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 100;
+        this.attack = 14;
+        this.accuracy = 9
+        this.defense = 6;
+        this.speed = 10;
+        this.maxLife = this.life;
+        this.alive = this.alive;
+    }
+}
