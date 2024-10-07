f8e1ee1f3488a38b6994bb183a4076a24d9c3c89
Pedro <apedro.rp@gmail.com>
2024-10-07 17:09:15 -0300

    feat new character Troll

---
+++ Jogo Luta/src/js/jogoLuta/characters/Troll/Troll.js
@@ -0,0 +1,14 @@
+import { Character } from "../Character.js";
+
+export class BigMonster extends Character {
+    constructor() {
+        super("Big Monster");
+        this.life = 150;
+        this.attack = 14;
+        this.accuracy = 3;
+        this.defense = 10;
+        this.speed = 12;
+        this.maxLife = this.life;
+        this.alive = this.alive;
+    }
+}
