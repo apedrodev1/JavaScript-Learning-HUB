51f2841b862dc48d977446285598a18b11c52a44
Pedro <apedro.rp@gmail.com>
2024-10-07 17:07:59 -0300

    feat new character Paladin

---
+++ Jogo Luta/src/js/jogoLuta/characters/Paladin/paladin.js
@@ -0,0 +1,14 @@
+import { Character } from "../Character";
+
+export class Paladin extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 150;
+        this.attack = 14;
+        this.accuracy = 4;
+        this.defense = 10;
+        this.speed = 7;
+        this.maxLife = this.life;
+        this.alive = this.alive;
+    }
+}
