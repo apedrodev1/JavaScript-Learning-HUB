9da8ec741539b5af60ec8b9e8d53b8ff9a27267f
Pedro <apedro.rp@gmail.com>
2024-11-27 23:32:50 -0300

    feat: create.php

--- To do list/database/conn.php
+++ To do list/database/conn.php
@@ -1,3 +1,4 @@
+
 <?php
 
 $hostname = 'localhost';
@@ -5,13 +6,8 @@ $database = 'to_do_list';
 $username = 'postgres';
 $password = '1234';
 
-
-try{
-    $pdo = new PDO("psgl:host $hostname, 
-    dbname = $database,
-    username = $username,
-    password = $password ");
-
+try {
+    $pdo = new PDO("pgsql:host=$hostname;dbname=$database", $username, $password);
 } catch (PDOException $e) {
-    echo "Erro: ". $e->getMessage();
+    echo "Erro: " . $e->getMessage();
 }
