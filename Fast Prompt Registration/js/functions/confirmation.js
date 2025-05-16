e9f0c92e0ab4d1154bfab83b25847d49588f8001
Pedro <apedro.rp@gmail.com>
2025-05-16 09:51:08 -0300

    renaming the project

--- Fast Prompt Registration/js/functions/confirmation.js
+++
@@ -1,8 +0,0 @@
-export function showConfirmation(user) {
-    alert(
-        `Thank you for registering, ${user.fullName}, You're in!!\n` +
-        `🎉 This is your lucky number: ${user.id}\n\n` +
-        `📧 Stay tuned to your email (${user.email}) for draw updates.\n` +
-        `Good luck! 🍀`
-    );
-}
--- Fast Prompt Registration/js/screenTwo.js
+++
@@ -1,3 +0,0 @@
-import main_function from './functions/mainFunction.js'
-
-main_function();
