65beaadd922e1486029c38421f9b703d0f2ef926
Pedro Augusto <apedro.rp@gmail.com>
2025-06-18 13:05:01 -0300

    test: mainFunctionV2.js

---
+++ QuickPrompt SignUp/src/js/screenGiveAway.js
@@ -0,0 +1,13 @@
+/**
+ * @file screenTwo.js
+ * @description
+ * Entry point for the second screen of the QuickPrompt SignUP application.
+ * 
+ * Currently, it imports and runs the monolithic mainFunctionV2 for testing purposes.
+ * Once testing is complete, it will switch to import and execute the modular mainFunction (v1).
+ */
+
+import mainFunctionV2 from './functions/mainFunctionV2.js';
+
+// Execute the main registration flow (monolithic version for now)
+mainFunctionV2();
